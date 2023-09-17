51d9796989f6f0cdf32e1d3056c2187118a19d08
geoffrey1330 <israelgeoffrey13@gmail.com>
2023-09-18 00:25:27 +0100

    Added cluster permissions

--- controllers/nginxoperator_controller.go
+++ controllers/nginxoperator_controller.go
@@ -39,6 +39,7 @@ type NginxOperatorReconciler struct {
 //+kubebuilder:rbac:groups=operator.geoffrey.com,resources=nginxoperators,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operator.geoffrey.com,resources=nginxoperators/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=operator.geoffrey.com,resources=nginxoperators/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
